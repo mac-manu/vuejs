202ab199cdf7bd1409f20e00ee02b947be63b78f
macmanu <manu@mac-manu.com>
2019-07-27 23:55:12 +0200

    exi

---
+++ Exercise 1/index.js
@@ -0,0 +1,13 @@
+new Vue({
+        el: '#exercise',
+        data: {
+            name: 'Manux ',
+            age: 37,
+            image: 'https://www.valuecoders.com/blog/wp-content/uploads/2016/11/Vue.js-cover.png'
+        },
+        methods: {
+        random: function() {
+          return Math.random();
+          }
+        }
+});
