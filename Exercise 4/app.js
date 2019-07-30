96abdbb35fb3ee61ce2cac59529161e041a74a2e
macmanu <manu@mac-manu.com>
2019-07-30 10:29:34 +0200

    ex4

---
+++ Exercise 4/app.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#exercise',
+  data: {
+    effectClasses: {
+      highlight: false,
+      shrink: true
+    }
+  },
+  methods: {
+    startEffect: function() {
+      setInterval(function){
+        
+      }
+
+    }
+  }
+});
